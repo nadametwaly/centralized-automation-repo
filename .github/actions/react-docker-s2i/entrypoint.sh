684e3063f98dc22a555b1b5532703c0ccf328ffb
Nada Metwaly <nmetwaly@yahoo.com>
2026-01-14 21:56:38 +0200

    comm

--- .github/actions/react-docker-s2i/entrypoint.sh
+++ .github/actions/react-docker-s2i/entrypoint.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 set -e
 
-REGISTRY="$INPUT_REGISTRY"
-USERNAME="$INPUT_USERNAME"
-PASSWORD="$INPUT_PASSWORD"
-IMAGE_NAME="$INPUT_IMAGE_NAME"
-TAG="$INPUT_TAG"
-SHA=$(echo "$GITHUB_SHA" | cut -c1-7)
+APP_PATH="$GITHUB_WORKSPACE/frontend"
+IMAGE_NAME="$INPUT_REGISTRY/$INPUT_USERNAME/$INPUT_IMAGE_NAME"
+SHA_TAG="$IMAGE_NAME:$(echo $GITHUB_SHA | cut -c1-7)"
+LATEST_TAG="$IMAGE_NAME:$INPUT_TAG"
 
-FULL_IMAGE="$REGISTRY/$USERNAME/$IMAGE_NAME"
-SHA_TAG="$FULL_IMAGE:$SHA"
-LATEST_TAG="$FULL_IMAGE:$TAG"
+echo "$INPUT_PASSWORD" | docker login "$INPUT_REGISTRY" -u "$INPUT_USERNAME" --password-stdin
 
-echo "🔐 Logging into $REGISTRY..."
-echo "$PASSWORD" | docker login "$REGISTRY" -u "$USERNAME" --password-stdin
-
-echo "🐳 Building Docker image..."
-docker build -t "$LATEST_TAG" -t "$SHA_TAG" .
-
-echo "📤 Pushing images..."
+docker build -t "$LATEST_TAG" -t "$SHA_TAG" "$APP_PATH"
 docker push "$LATEST_TAG"
 docker push "$SHA_TAG"
 
@@ -27,5 +17,3 @@ DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' "$SHA_TAG" | cut -d'
 echo "image_name=$SHA_TAG" >> $GITHUB_OUTPUT
 echo "image_digest=$DIGEST" >> $GITHUB_OUTPUT
 echo "push_status=success" >> $GITHUB_OUTPUT
-
-echo "✅ Image pushed: $SHA_TAG"
